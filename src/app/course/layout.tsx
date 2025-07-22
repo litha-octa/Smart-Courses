6c18337fd2f2522bb6204a791d56a3977a43e1d5
dian <dianpayno2023@gmail.com>
2025-07-22 07:05:28 +0700

    dian-detail-page

---
+++ src/app/course/layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Navbar } from '@/components/ui'
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+const SmartCourseLayout = ({ children }: LayoutProps) => {
+  return (
+    <div>
+      <Navbar/>
+      <div className='bg-gray-100'>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default SmartCourseLayout
---
+++ src/app/data/listCourse.ts
@@ -0,0 +1,29 @@
+export const listCourse = [
+  {
+    id: 1,
+    title: "Debian & Ubuntu Server Administration",
+    subtitle:
+      "Belajar konfigurasi Debian dan Ubuntu Server dengan kurikulum yang disusun sesuai kebutuhan industri!",
+    author: "Ahmad Rosid Komarudin",
+    rating: "4.5",
+    category: "Tech",
+    videolink:
+      "https://www.youtube.com/watch?v=nXXqs5-dYsA&list=PLSmk1Em-R3rqlsmL6WPw0DJmRBMc59SbK",
+  },
+];
+
+export const detailCourse = {
+  id: 1,
+  title: "Debian & Ubuntu Server Administration",
+  subtitle:
+    "Belajar konfigurasi Debian dan Ubuntu Server dengan kurikulum yang disusun sesuai kebutuhan industri!",
+  author: "Ahmad Rosid Komarudin",
+  rating: "4.5",
+  category: "Tech",
+  thumbnailUrl: "https://img.youtube.com/vi/nXXqs5-dYsA/hqdefault.jpg",
+  videolink:
+    "https://www.youtube.com/embed/nXXqs5-dYsA?autoplay=1",
+    views: 1000
+};
+
+export const menuDetail =['Detail', 'Kurikulum', 'Mentor', 'Ulasan']
